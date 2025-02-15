c74dec5e64ceea9dbe58dcaed81d2b187bdae427
denizhankara <kara4@illinois.edu>
2025-02-15 01:48:51 -0600

    Change hardhat.config.ts localost url

--- packages/bundler/hardhat.config.ts
+++ packages/bundler/hardhat.config.ts
@@ -35,7 +35,7 @@ const config: HardhatUserConfig = {
   },
   networks: {
     localhost: {
-      url: 'http://localhost:8545/',
+      url: 'http://127.0.0.1:8545/',
       saveDeployments: false
     },
     goerli: getInfuraNetwork('goerli')
